0e5d6cc672815e70adacfe0c6dd42e80dd789ac5
BhavyaGaharwar <bhavya0653@gmail.com>
2026-09-01 11:30:50 +0530

    commit

---
+++ reqmethod.js
@@ -0,0 +1,9 @@
+const http=require("http");
+const server=http.createServer((req,res)=>{
+    console.log(req.method);
+    res.end("Request received");
+});
+
+server.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
