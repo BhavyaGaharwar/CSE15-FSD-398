2f4a5073603c07fb9f4524112fe34110f5395ef4
BhavyaGaharwar <bhavya0653@gmail.com>
2026-09-16 14:29:25 +0530

    eventEmitter

---
+++ EventEmitter/eventEmitter.js
@@ -0,0 +1,26 @@
+// Using Node.js built-in events module,create an EventEmitter. Register multiple listener's for a response event,
+// then emit the event by passing name and id as arguments and display them in the console.
+
+// import http from "http";
+// import EventEmitter from "events";
+
+// const myEmitter= new EventEmitter();
+// myEmitter.on("response",() => {
+//     console.log("data received");
+// });
+
+
+import EventEmitter from "events";
+const myEmitter = new EventEmitter();
+
+myEmitter.on("response", (name, id) => {
+    console.log("Data received");
+    console.log("Name:", name);
+    console.log("ID:", id);
+});
+
+myEmitter.on("response", (name, id) => {
+    console.log("Response event received");
+});
+
+myEmitter.emit("response", "Bhavya", 101);
